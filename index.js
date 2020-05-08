a6d677019337a6b3268de645de09ca7b6fb2a505
Mikael-R <mikarg9@gmail.com>
2020-05-08 20:37:37 -0300

    Refatorado

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ require('dotenv/config')
 client.login(process.env.TOKEN)
 
 client.on('ready', () => {
-    console.log(new Date().toLocaleString('pt-BR', { timeZome: 'UTC-3'}) + ' Online')
+    console.log(new Date().toLocaleString('pt-BR') + ' Online')
 })
 
 client.on('message', msg => {
-    if (msg.content.toLocaleLowerCase().startsWith('$ajuda')) {
+    if (msg.content.toLocaleLowerCase() == '/mixmods ajuda') {
         msg.reply(`**
                                                     [Mixmods-Notifier]
-:ballot_box_with_check: $ajuda: Informa os comandos para utilização.
+:ballot_box_with_check: /mixmods ajuda: Informa os comandos para utilização.
 
-:ballot_box_with_check: $dataAtual: Informa a data juntamente com a hora(UTC-3).
+:ballot_box_with_check: /mixmods data: Informa a data juntamente com a hora(UTC-3).
 **`)
     }
-})
-
-client.on('message', msg => {
-    if (msg.content.toLocaleLowerCase().startsWith('$dataatual')) {
-        msg.reply(new Date().toLocaleString('pt-BR', { timeZome: 'UTC-3'}))
+    if (msg.content.toLocaleLowerCase() == '/mixmods data') {
+        msg.reply(new Date().toLocaleString('pt-BR'))
     }
 })
