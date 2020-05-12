c9a86e6398958ed595c34aff1a325ff61d8810cf
Mikael-R <mikarg9@gmail.com>
2020-05-12 09:52:28 -0300

    Adicionado code-style

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
 # MixMods-Notifier
 Notifica sobre novos posts da mixmods
 
--- index.js
+++ index.js
@@ -5,28 +5,32 @@ require('dotenv/config')
 const client = new Discord.Client()
 const parser = new RssParser()
 
-client.on('ready', () => { // informações do bot quando está online //
-    console.log(`[${new Date().toLocaleString('pt-BR')}] Logado como ${client.user.tag}`)
+client.on('ready', () => {
+  // informações do bot quando está online //
+  console.log(`[${new Date().toLocaleString('pt-BR')}] Logado como ${client.user.tag}`)
 })
 
-client.on('message', async msg => { // informa os comandos //
-    let embed = new Discord.MessageEmbed().setTitle('[Mixmods-Notifier]').setColor('#4e4784') // definições do embed //
+client.on('message', async (msg) => {
+  // informa os comandos //
+  const feed = await parser.parseURL('https://www.mixmods.com.br/feeds/posts/default?alt=rss')
+  const embed = new Discord.MessageEmbed().setTitle('[Mixmods-Notifier]').setColor('#4e4784') // definições do embed //
 
-    if (msg.content.toLocaleLowerCase().split(' ')[0] != '/mixmods') { // verifica se a mensagem é um comando //
-        return
-    }
-    else if (msg.content.toLocaleLowerCase() == '/mixmods ping') { // informa o ping do servidor e da api //
-        embed.setDescription(`
+  if (msg.content.toLocaleLowerCase().split(' ')[0] !== '/mixmods') {
+    // verifica se a mensagem é um comando //
+    return null
+  } else if (msg.content.toLocaleLowerCase() === '/mixmods ping') {
+    // informa o ping do servidor e da api //
+    embed.setDescription(`
 :ping_pong: Pong!
 
-:purple_circle: Server: ${Date.now() - msg.createdTimestamp} ms
+:purple_circle: Server: ${Date.now() - msg.createdTimestamp}ms
 
-:purple_circle: Api: ${client.ws.ping} ms
+:purple_circle: Api: ${client.ws.ping}ms
         `)
-        msg.channel.send(embed)
-    }
-    else if (msg.content.toLocaleLowerCase() == '/mixmods ajuda') { // informa os comandos //
-        embed.setDescription(`
+    msg.channel.send(embed)
+  } else if (msg.content.toLocaleLowerCase() === '/mixmods ajuda') {
+    // informa os comandos //
+    embed.setDescription(`
 :purple_circle: **/mixmods ajuda**: Informa os comandos para utilização.
 
 :purple_circle: **/mixmods ping**: Informa o ping do servidor e da api.
@@ -39,59 +43,58 @@ client.on('message', async msg => { // informa os comandos //
 
 :purple_circle: **/mixmods links**: Informa os links para o site e o fórum.
         `)
-        msg.channel.send(embed)
-    }
-    else if (msg.content.toLocaleLowerCase() == '/mixmods data') { // informa a data //
-        embed.setDescription(`Neste momento estou operando no dia ${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()} as ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
-        msg.channel.send(embed)
-    }
-    else if (msg.content.toLocaleLowerCase() == '/mixmods posts') { // informa os 4 últimos posts //
-        let feed = await parser.parseURL('https://www.mixmods.com.br/feeds/posts/default?alt=rss')
-        for (let n = 0; n < 4; n++) { // máximo 25 posts(0 a 24) //
-            let categorias = '| '
-            for (let c in feed.items[n].categories) {
-              categorias += feed.items[n].categories[c]._ + ' | '
-            }
-            embed.setDescription(`
+    msg.channel.send(embed)
+  } else if (msg.content.toLocaleLowerCase() === '/mixmods data') {
+    // informa a data //
+    embed.setDescription(`Neste momento estou operando no dia ${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()} as ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
+    msg.channel.send(embed)
+  } else if (msg.content.toLocaleLowerCase() === '/mixmods posts') {
+    // informa os 4 últimos posts //
+    // const feed = await parser.parseURL('https://www.mixmods.com.br/feeds/posts/default?alt=rss')
+    for (let n = 0; n < 4; n++) {
+      // máximo 25 posts(0 a 24) //
+      let categorias = '| '
+      for (const c in feed.items[n].categories) {
+        categorias += feed.items[n].categories[c]._ + ' | '
+      }
+      embed.setDescription(`
 :purple_circle: **Título**: ${feed.items[n].title}
 
 :purple_circle: **Link**: ${feed.items[n].link}
 
 :purple_circle: **Categorias**: ${categorias}
 
-:purple_circle: **Publicado**: ${feed.items[n].pubDate.substr(5,3) + ' ' + feed.items[n].pubDate.substr(8,9)}
-            `)
-            msg.channel.send(embed)
-          }
+:purple_circle: **Publicado**: ${
+        feed.items[n].pubDate.substr(5, 3) + ' ' + feed.items[n].pubDate.substr(8, 9)}`)
+      msg.channel.send(embed)
     }
-    else if (msg.content.toLocaleLowerCase() == '/mixmods post') { // informa o último post //
-        let feed = await parser.parseURL('https://www.mixmods.com.br/feeds/posts/default?alt=rss')
-        let categorias = '| '
-        for (let c in feed.items[0].categories) {
-            categorias += feed.items[0].categories[c]._ + ' | '
-        }
-        embed.setDescription(`
+  } else if (msg.content.toLocaleLowerCase() === '/mixmods post') { // informa o último post //
+    // const feed = await parser.parseURL('https://www.mixmods.com.br/feeds/posts/default?alt=rss')
+    let categorias = '| '
+    for (const c in feed.items[0].categories) {
+      categorias += feed.items[0].categories[c]._ + ' | '
+    }
+    embed.setDescription(`
 :purple_circle: **Título**: ${feed.items[0].title}
 
 :purple_circle: **Link**: ${feed.items[0].link}
 
 :purple_circle: **Categorias**: ${categorias}
 
-:purple_circle: **Publicado**: ${feed.items[0].pubDate.substr(5,3) + ' ' + feed.items[0].pubDate.substr(8,9)}
+:purple_circle: **Publicado**: ${feed.items[0].pubDate.substr(5, 3) + ' ' + feed.items[0].pubDate.substr(8, 9)}
 `)
-        msg.channel.send(embed)
-    }
-    else if (msg.content.toLocaleLowerCase() == '/mixmods links') {
-        embed.setDescription(`
+    msg.channel.send(embed)
+  } else if (msg.content.toLocaleLowerCase() === '/mixmods links') {
+    embed.setDescription(`
 :purple_circle: Site: https://mixmods.com.br
 
 :purple_circle: Fórum: https://forum.mixmods.com.br
         `)
-        msg.channel.send(embed)
-    } else {
-        embed.setDescription(`:purple_circle: Comando inválido, use **/mixmods ajuda** para ver a lista de comandos.`)
-        msg.channel.send(embed)
-    }
+    msg.channel.send(embed)
+  } else {
+    embed.setDescription(':purple_circle: Comando inválido, use **/mixmods ajuda** para ver a lista de comandos.')
+    msg.channel.send(embed)
+  }
 })
 
 client.login(process.env.TOKEN)
